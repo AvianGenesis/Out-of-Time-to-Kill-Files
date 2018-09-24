a261f7c00729aef09e63f8311492b2995626ae14
zflahert <zflahert@trinity.edu>
2018-09-24 03:38:41 -0500

    Init Commit
    + player movement
    + screen border
    * polish sprite rendering

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float flow;
+    public float speed;
+    public float velX;
+    public float velY;
+    public float score;
+    public float energy;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        flow = 1f;
+        speed = 7f;
+
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        velX = inputX * speed * flow;
+        velY = inputY * speed * flow;
+
+        rb.velocity = new Vector2(velX, velY);
+
+	}
+}
